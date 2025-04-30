ec2bb9514c52a27da5b074e608251112378215de
san <cjsdjsdl@proton.me>
2025-05-01 00:31:03 +0900

    refactor code

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -5,9 +5,18 @@ import camp.nextstep.edu.missionutils.Randoms
 class Car(val name: String) {
     var position: Int = 0
 
+    init {
+        require(name.isNotBlank()) {"자동차 이름을 비어있지 않게 입력해주세요"}
+        require(name.length <= 5) {"자동차 이름을 5자 이하로 입력해주세요"}
+    }
+
     fun moveRandomly() {
         if (Randoms.pickNumberInRange(0, 9) >= 4) {
             position++
         }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return name === (other as? Car)?.name
     }
 }
--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -4,27 +4,30 @@ import camp.nextstep.edu.missionutils.Console
 
 class Racing {
     private val cars = arrayListOf<Car>()
-    private var tryCount: Int
+    private val tryCount: Int
     private val winners = arrayListOf<Car>()
 
     init {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
         Console.readLine()
             .split(",")
-            .forEach { name ->
-                if (name.isEmpty() || name.length > 5) {
-                    throw IllegalArgumentException("자동차 이름을 5자 이하로 입력해주세요")
-                }
-                cars.add(Car(name))
-            }
+            .forEach { addCar(it) }
 
         println("시도할 횟수는 몇 회인가요?")
-        tryCount = Console.readLine()
-            ?.toIntOrNull()
-            ?: throw IllegalArgumentException("시도할 횟수를 숫자로 입력하세요")
+        tryCount = Console.readLine()?.toIntOrNull()
+            ?: throw IllegalArgumentException("시도 횟수를 입력해주세요")
+        require(tryCount > 0) {"시도 횟수는 1 이상이어야 합니다"}
+    }
+
+    private fun addCar(name: String) {
+        val newCar = Car(name)
+        require(cars.indexOf(newCar) == -1) {"자동차 이름이 중복됩니다"}
+        cars.add(newCar)
     }
 
     fun start() {
+        require(cars.size > 1) {"혼자만의 레이스는 외롭잖아요"}
+
         for (i in 0 until tryCount) {
             moveCars()
             println()
@@ -32,13 +35,13 @@ class Racing {
 
         calculateWinners()
 
-        println("최종 우승자: ${winners.joinToString(", ") { it.name }}")
+        println("최종 우승자 : ${winners.joinToString(", ") { it.name }}")
     }
 
     private fun moveCars() {
         cars.forEach {
             it.moveRandomly()
-            println("${it.name} : ${it.position}")
+            println("${it.name} : ${"-".repeat(it.position)}")
         }
     }
 
